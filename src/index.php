2e11c4193058a7e3da0e779e4a2b45682e94c699
wein1m <weynn.sz@gmail.com>
2026-08-10 17:11:58 +0800

    refactor: separate header & footer into PHP components

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once "head.php";
+?>
+
+<div class="bg-bg w-full h-screen flex items-center justify-center">
+    <h1 class="font-grotesk font-medium text-9xl text-text-primary">
+        Helllo World~!
+    </h1>
+</div>
+
+<?php
+include_once "foot.php";
+?>
