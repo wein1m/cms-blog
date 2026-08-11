67ccdfe7052dbb66f495ea1d5299045c6ecdd894
wein1m <weynn.sz@gmail.com>
2026-08-11 21:17:11 +0800

    chore: make links accessible in every levels

--- src/head.php
+++ src/head.php
@@ -4,14 +4,16 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="output.css" />
+    <link rel="stylesheet" href="/cms-blog/src/output.css" />
 </head>
 
 <body>
 
     <nav class="sticky top-0 z-[999] w-full flex items-center justify-between px-24 py-4 border-b bg-bg border-black/10">
         <div class="flex items-center">
-            <img src="../assets/logo/shiori-nobg.png" class="h-16 mr-10" />
+            <a href="/cms-blog/src/index.php">
+                <img src="/cms-blog/assets/logo/shiori-nobg.png" class="h-16 mr-10" />
+            </a>
             <ul>
                 <li class="flex gap-8 text-lg tracking-wider font-medium">
                     <a href="#">Home</a>
@@ -24,7 +26,7 @@
         <a>Login / Register</a>
       </div> -->
         <div class="">
-            <a href="./new-story.php" class="flex items-center gap-2 bg-primary text-white text-lg tracking-wider font-bold px-6 py-3">
+            <a href="./new-story" class="flex items-center gap-2 bg-primary text-white text-lg tracking-wider font-bold px-6 py-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                     <path fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M12 5v14m-7-7h14" />
